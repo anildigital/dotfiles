ccc068acef3cd3f09d8cb0a00b771e42ef6558e0
Anil Wadghule <anildigital@gmail.com>
2010-07-22 22:32:28 +0530

    More scripts

--- gemmate.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-if ARGV.empty?
-  puts "Usage: gemmate <name-of-gem>"
-else
-  src     = `gem which #{ARGV[0]}`.split.last
-  lib_dir = src.slice 0, src.rindex("lib")
-  `mate -a '#{lib_dir}'`
-end
